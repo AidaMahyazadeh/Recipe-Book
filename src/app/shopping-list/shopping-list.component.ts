d0c624ae33715adfa0fac1e991bacf0cc41f3b67
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-11 10:50:38 +0200

    imported ingredient model.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import IIngredient from '../models/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients :IIngredient [] = [
+    {name :'apple', amount :10}
+  ];
 }
