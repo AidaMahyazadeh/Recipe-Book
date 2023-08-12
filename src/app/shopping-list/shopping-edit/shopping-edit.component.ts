56125eb4f3890af14d4dbcf3d3078943b60bc792
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-12 12:58:20 +0200

    wrote onAddItem function.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import {IIngredient} from 'src/app/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+@ViewChild('nameInput') nameInputRef !:ElementRef ;
+@ViewChild('amountInput') amountInputRef !:ElementRef ;
+@Output() ingredientAdded = new EventEmitter <IIngredient>();
 
+onAddItem(){
+ const ingName :string = this.nameInputRef.nativeElement.value;
+ const ingAmount :number = this.amountInputRef.nativeElement.value;
+ const detailIng = new IIngredient(ingName,ingAmount)
+ this.ingredientAdded.emit(detailIng)
+}
 }
