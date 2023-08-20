9105d5d1918042807789f214822de498bc33f7a9
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-20 12:31:10 +0200

    imported IRecipe.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import IRecipe from '../shared/models/recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent {
+  selectedRecipe !: IRecipe;
 
+  ongetSelectedRecipe(recipe :IRecipe){
+   this.selectedRecipe=recipe
+  }
 }
