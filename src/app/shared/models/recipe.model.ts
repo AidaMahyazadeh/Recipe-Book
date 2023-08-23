f79f8e8f81a2a27aa8a21ee23dd3ac933fe08ab8
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-23 12:34:54 +0200

    added ingredients property to interface.

--- src/app/shared/models/recipe.model.ts
+++ src/app/shared/models/recipe.model.ts
@@ -1,5 +1,8 @@
+import { IIngredient } from "./ingredient.model";
+
 export default interface IRecipe {
     name:string;
     description :string;
     imagePath : string;
+    ingredients : IIngredient[];
 }
