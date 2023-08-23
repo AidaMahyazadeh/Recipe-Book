2c223747a235088b4eaf1f67b45cbe3b4ab17c58
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-23 17:26:04 +0200

    replaced EventEmitter with Subject.

--- src/app/shared/services/shopping.service.ts
+++ src/app/shared/services/shopping.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { IIngredient } from '../models/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingService {
-
+ private ingredients :IIngredient [] = [
+    {name :'apple', amount :10}
+  ];
+  ingredientChanged = new Subject <IIngredient[]>();
   constructor() { }
-}
+
+  getIngredients (){
+    return this.ingredients.slice();
+  }
+
+  addIngredient (ingredient :IIngredient){
+    this.ingredients.push(ingredient);
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
+
+  addIngredients (ingredients :IIngredient[]){
+    this.ingredients.push (...ingredients);
+    this.ingredientChanged.next(this.ingredients.slice());
+    }
+
+  }
+
