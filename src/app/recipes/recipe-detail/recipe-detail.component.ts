d3b0233a873103b296044c0e5144e34114b86e3f
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-23 12:19:13 +0200

    showing recipe detail dynamically.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import IRecipe from 'src/app/shared/models/recipe.model';
+import { RecipeService } from 'src/app/shared/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
-@Input() recipe !:IRecipe ;
+export class RecipeDetailComponent implements OnInit {
+recipe !:IRecipe ;
+id !:number ;
+constructor (
+  private recipeService :RecipeService,
+  private activatedRoute :ActivatedRoute
+  ){}
+
+ngOnInit(): void {
+  this.activatedRoute.params.subscribe(
+    (params :Params) =>{
+      this.id = Number(params['id']);
+      this.recipe = this.recipeService.getRecipeById(this.id);
+    }
+  );
+}
+
+onAddToShoppingList(){
+this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+}
 }
