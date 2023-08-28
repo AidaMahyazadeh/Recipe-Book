1965ded701bc37b3bd0378b5e64d87ea24b61d9f
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-28 12:34:49 +0200

    created method for storing recipes in firebase.

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataStorageService {
 
-  constructor() { }
+export class DataStorageService {
+url ='https://recipe-app-99d4d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json';
+
+  constructor(
+    private http :HttpClient,
+    private recipeService :RecipeService
+    ) { }
+ 
+storeRecipes(){
+  const recipes = this.recipeService.getRecipes();
+  this.http.put(this.url,recipes).subscribe(
+    response =>{
+      console.log(response)
+    }
+  )
+}
+
 }
