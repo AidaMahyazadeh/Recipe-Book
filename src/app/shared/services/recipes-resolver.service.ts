aa8bbb05c98fbccefc346d32f7cfdb2f23229909
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-29 17:15:12 +0200

    fetch data before navigating.

---
+++ src/app/shared/services/recipes-resolver.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { DataStorageService } from './data-storage.service';
+import { RecipeService } from './recipe.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesResolverService {
+
+  constructor(
+    private dataStorageService :DataStorageService,
+    private recipeService :RecipeService
+    ) { }
+
+  resolve(){
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length ===0){
+      return this.dataStorageService.fetchRecipes()
+    }else{
+      return recipes;
+    }
+  }
+}
