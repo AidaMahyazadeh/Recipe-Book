4bf40649fb276e1f48d0c8ca9c0450e281746567
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-29 17:13:18 +0200

    reset form.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { ISignup } from '../shared/models/signup.model';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+isLoginMode = true;
+isLoading =false;
+error !:string;
 
+constructor(private authService :AuthService){}
+
+onSwitchMode (){
+  this.isLoginMode = !this.isLoginMode;
+}
+
+onSubmit(form ){
+  if (form.invalid){
+    return;
+  }
+
+  const email = form.value.email;
+  const password = form.value.password;
+  this.isLoading =true;
+  if (this.isLoginMode){
+
+  }else{
+    this.authService.signup(email,password).subscribe(
+      resData=>{
+        console.log(resData)
+        this.isLoading =false;
+      },
+      errorMessage =>{
+        console.log( errorMessage)
+        this.error =  errorMessage.message;
+        this.isLoading =false;
+      }
+    )
+  }
+   
+form.reset()
+}
 }
