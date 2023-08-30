1bb755a565c771c95351422dcb81547381c758ae
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-30 12:35:13 +0200

    activated logout button.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,29 @@
-import { Component} from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { DataStorageService } from '../shared/services/data-storage.service';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-constructor(private dataStorageService :DataStorageService ){}
+export class HeaderComponent implements OnInit, OnDestroy{
+ private userSub !:Subscription;
+ isAuthenticated = false;
+
+constructor(
+  private dataStorageService :DataStorageService,
+  private authService :AuthService
+  ){}
+
+  ngOnInit(): void {
+  this.userSub  = this.authService.user$.subscribe(
+    user =>{
+      this.isAuthenticated = !user ? false : true;
+    }
+  )
+  }
 
 onSaveData(){
   this.dataStorageService.storeRecipes();
@@ -15,5 +31,13 @@ onSaveData(){
 
 onFetchData(){
   this.dataStorageService.fetchRecipes().subscribe()
+}
+
+onLogout(){
+  this.authService.logout();
+}
+
+ngOnDestroy(): void {
+  this.userSub.unsubscribe();
 }
 }
