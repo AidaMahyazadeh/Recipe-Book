aa3574ea81cc59fb7c077ed5d32030b61a2191a7
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-30 12:04:12 +0200

    provide interceptor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +12,7 @@ import { RecipesModule } from './recipes/recipes.module';
 import { ShoppongListModule } from './shopping-list/shopping-list.module';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
+import { AuthInterceptor } from './shared/services/auth.interceptor';
 
 
 
@@ -32,7 +33,13 @@ import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loa
     RecipesModule,
     ShoppongListModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass : AuthInterceptor,
+      multi : true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
