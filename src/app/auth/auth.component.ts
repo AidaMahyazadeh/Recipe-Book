a8c898fac08cf02556a30fd817b85caeac4b5cc5
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-30 10:55:17 +0200

    after authentication navigate to recipes route.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
+import {  Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { AuthService } from '../shared/services/auth.service';
-import { ISignup } from '../shared/models/signup.model';
+import { IAuthResponseData } from '../shared/models/authResponseData.model';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,10 @@ isLoginMode = true;
 isLoading =false;
 error !:string;
 
-constructor(private authService :AuthService){}
+constructor(
+  private authService :AuthService,
+  private router :Router
+  ){}
 
 onSwitchMode (){
   this.isLoginMode = !this.isLoginMode;
@@ -26,22 +30,27 @@ onSubmit(form ){
 
   const email = form.value.email;
   const password = form.value.password;
+  let authObs : Observable <IAuthResponseData>;
+
   this.isLoading =true;
   if (this.isLoginMode){
-
+    authObs = this.authService.login(email,password)
   }else{
-    this.authService.signup(email,password).subscribe(
-      resData=>{
-        console.log(resData)
-        this.isLoading =false;
-      },
-      errorMessage =>{
-        console.log( errorMessage)
-        this.error =  errorMessage.message;
-        this.isLoading =false;
-      }
-    )
+   authObs = this.authService.signup(email,password)
   }
+  authObs.subscribe({
+    next :(resData)=>{
+     console.log(resData)
+     this.isLoading =false;
+     this.router.navigate(['/recipes']);
+   },
+   error :(errorMessage) =>{
+     console.log( errorMessage)
+     this.error =  errorMessage.message;
+     this.isLoading =false;
+   }
+ }
+ )
    
 form.reset()
 }
