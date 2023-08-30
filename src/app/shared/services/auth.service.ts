560ec46495fe014ad6ab54801696c2e00a88a599
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-08-30 09:48:25 +0200

    created login method.

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -7,13 +7,15 @@ import { catchError, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-url=`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=
+urlSignup=`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=
 AIzaSyB5DPFXGWG8NitlmtRRXpR3Y8sha9cwdKQ`;
+urlLogin ='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB5DPFXGWG8NitlmtRRXpR3Y8sha9cwdKQ'
+
   constructor(private http :HttpClient) { }
 
   signup(email: string, password: string){
    return this.http.post<IAuthResponseData
-   >(this.url, {email,password,returnSecureToken:true})
+   >(this.urlSignup, {email,password,returnSecureToken:true})
    .pipe(
     catchError(errorRespone  =>{
       let errorMessage = 'An unknown error occured!'
@@ -22,10 +24,38 @@ AIzaSyB5DPFXGWG8NitlmtRRXpR3Y8sha9cwdKQ`;
         ) ;
       }
       switch(errorRespone.error.error.message){
-        case 'EMAIL_EXISTS' : errorMessage ='This email already exists.'; 
+        case 'EMAIL_EXISTS' : errorMessage ='This email already exists.';
+        break;
+        case 'EMAIL_NOT_FOUND' : errorMessage ='This email does not exist.'
+        break;
+        case 'INVALID_PASSWORD' : errorMessage = 'Password is not correct.'
+        break;
       }
       return throwError (()=>new Error (errorMessage));
     })
    )
+  }
+
+
+  login(email: string, password: string){
+   return  this.http.post<IAuthResponseData
+     >(this.urlLogin, {email,password,returnSecureToken:true}).pipe(
+      catchError (errorRespone  =>{
+        let errorMessage = 'An unknown error occured!'
+        if(!errorRespone.error || !errorRespone.error.error){
+          return throwError (()=>new Error (errorMessage)
+          ) ;
+        }
+        switch(errorRespone.error.error.message){
+          case 'EMAIL_EXISTS' : errorMessage ='This email already exists.'; 
+          break;
+          case 'EMAIL_NOT_FOUND' : errorMessage ='This email does not exist.'
+          break;
+          case 'INVALID_PASSWORD' : errorMessage = 'Password is not correct.'
+          break;
+        }
+        return throwError (()=>new Error (errorMessage));
+      })
+     )
   }
 }
