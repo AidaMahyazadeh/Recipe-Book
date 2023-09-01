2d62340ea45305535baaba1dd3f8c566c2cbccbf
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-01 12:20:08 +0200

    created enviroment fils and put APIKEY.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment ={
+    production : true,
+    firebaseAPIKey : 'AIzaSyB5DPFXGWG8NitlmtRRXpR3Y8sha9cwdKQ'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment ={
+    production : false,
+    firebaseAPIKey : 'AIzaSyB5DPFXGWG8NitlmtRRXpR3Y8sha9cwdKQ'
+}
